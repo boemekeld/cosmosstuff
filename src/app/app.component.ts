e00d68b805bc7f42dd78433cfec87e80771e175d
MateusMo <mat.marq.oliveira@gmail.com>
2022-10-02 15:05:48 -0300

    google analytics implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd} from '@angular/router';
+import { filter } from 'rxjs';
+declare const gtag: Function; // <------------Important: the declartion for gtag is required!
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'cosmosstuff';
+  constructor(private router:Router) {
+    /** START : Code to Track Page View using gtag.js */
+    this.router.events.pipe(
+      filter((event): event is NavigationEnd => event instanceof NavigationEnd)
+    ).subscribe((event: NavigationEnd) => {
+       gtag('event', 'page_view', {
+          page_path: event.urlAfterRedirects
+       })
+      })
+      /** END : Code to Track Page View  using gtag.js */      
+  }
+
+  ngOnInit() {
+    window.scrollTo(0, 0);
+  }
 }
